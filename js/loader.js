e57a16fe72c579a761715b9d7b9a5469c5c5a667
Shombith03 <shombithofficial@outlook.com>
2025-04-28 15:22:50 +0530

    Added Site Loader

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+// loader.js
+window.addEventListener("load", () => {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block";
+});
